e42ba3e0c36d3d5d6300246b81630f4b7bb62598
Tarik Abdalla <tnabdall@ucalgary.ca>
2019-08-31 20:43:48 -0600

    Tested contact information classes.

---
+++ UniversityPeopleTests/ContactInformations/FacultyContactInformationTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniversityPeople.ContactInformations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityPeople.ContactInformations.Tests
+{
+    [TestClass()]
+    public class FacultyContactInformationTests
+    {
+        [TestMethod()]
+        public void FacultyContactInformation_ValidParameters()
+        {
+            //Arrange
+            String emailAddress = "bob@gmail.com";
+            String buildingLocation = "MC 206";
+            
+            //Act
+            FacultyContactInformation contactInformation = new FacultyContactInformation(emailAddress, buildingLocation);
+
+            //Assert
+            Assert.AreEqual(emailAddress, contactInformation.EmailAddress);
+            Assert.AreEqual(buildingLocation, contactInformation.BuildingLocation);
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FacultyContactInformation_InvalidEmail1()
+        {
+            //Arrange
+            String emailAddress = "bobxgmail.com";
+            String buildingLocation = "MC 206";
+
+            //Act
+            FacultyContactInformation contactInformation = new FacultyContactInformation(emailAddress, buildingLocation);
+
+            //Assert
+            Assert.Fail();
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FacultyContactInformation_InvalidEmail2()
+        {
+            //Arrange
+            String emailAddress = "bob@gmailxcom";
+            String buildingLocation = "MC 206";
+
+            //Act
+            FacultyContactInformation contactInformation = new FacultyContactInformation(emailAddress, buildingLocation);
+
+            //Assert
+            Assert.Fail();
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FacultyContactInformation_NoEmail()
+        {
+            //Arrange
+            String emailAddress = "";
+            String buildingLocation = "MC 206";
+
+            //Act
+            FacultyContactInformation contactInformation = new FacultyContactInformation(emailAddress, buildingLocation);
+
+            //Assert
+            Assert.Fail();
+
+        }
+
+        
+    }
+}
---
+++ UniversityPeopleTests/ContactInformations/StudentContactInformationTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniversityPeople.ContactInformations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityPeople.ContactInformations.Tests
+{
+    [TestClass()]
+    public class StudentContactInformationTests
+    {
+        [TestMethod()]
+        public void StudentContactInformation_ValidParameters()
+        {
+            //Arrange
+            String emailAddress = "bob@gmail.com";
+            String mailingAddress = "123 Centre St";
+
+            //Act
+            StudentContactInformation contactInformation = new StudentContactInformation(emailAddress,mailingAddress);
+
+            //Assert
+            Assert.AreEqual(emailAddress, contactInformation.EmailAddress);
+            Assert.AreEqual(mailingAddress, contactInformation.MailingAddress);
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void StudentContactInformation_InvalidEmail1()
+        {
+            //Arrange
+            String emailAddress = "bobxgmail.com";
+            String mailingAddress = "123 Centre St";
+
+            //Act
+            StudentContactInformation contactInformation = new StudentContactInformation(emailAddress, mailingAddress);
+
+            //Assert
+            Assert.Fail();
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void StudentContactInformation_InvalidEmail2()
+        {
+            //Arrange
+            String emailAddress = "bob@gmailxcom";
+            String mailingAddress = "123 Centre St";
+
+            //Act
+            StudentContactInformation contactInformation = new StudentContactInformation(emailAddress, mailingAddress);
+
+            //Assert
+            Assert.Fail();
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void StudentContactInformation_NoEmail()
+        {
+            //Arrange
+            String emailAddress = "";
+            String mailingAddress = "123 Centre St";
+
+            //Act
+            StudentContactInformation contactInformation = new StudentContactInformation(emailAddress, mailingAddress);
+
+            //Assert
+            Assert.Fail();
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void StudentContactInformation_NoMailingAddress()
+        {
+            //Arrange
+            String emailAddress = "bob@gmail.com";
+            String mailingAddress = "";
+
+            //Act
+            StudentContactInformation contactInformation = new StudentContactInformation(emailAddress, mailingAddress);
+
+            //Assert
+            Assert.Fail();
+
+        }
+    }
+}
